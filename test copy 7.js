51497517d6af11eeb727823ec74a8200220ad45f
longjiealice_v <longjiealice_v@didiglobal.com>
2024-10-21 01:01:27 +0800

    x

--- test copy 7.js
+++ test copy 7.js
@@ -1,7 +1,7 @@
 
 
 let s = 'lj.daiban.p.privacy.sign'
-let s = 'lj.daiban.p.privacy.xx'
+
 function go() {
     let e = 'lj.xx.di.x.oo'
 }
--- test.sh
+++ test.sh
@@ -1,25 +1,33 @@
-#!/bin/bash
+#! /usr/bin/env node
+const { exec } = require('child_process');
+const fs = require('fs');
+const path = require('path');
 
-# 获取当前分支名
-CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-echo "当前分支名: $CURRENT_BRANCH"
+exec('git rev-parse --abbrev-ref HEAD', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`执行错误: ${error}`);
+    return;
+  }
+  const currentBranch = stdout.trim();
+  console.log(`当前分支名: ${currentBranch}`);
 
-# 定义接口匹配的正则表达式
-# 获取本次提交和远程master分支的差异
-DIFF=$(git diff --cached origin/main)
-echo "比对获取到的差异代码：" $DIFF
+  // 定义接口匹配的正则表达式
+  const interfaceRegex = /lj\.\w+\.\w+\.\w+\.\w+/g;
 
-# 搜索符合模式的字符串
-matched_strings=()
-INTERFACE_REGEX="lj\.\w+\.\w+\.\w+\.\w+"
-NEW_INTERFACES=$(echo "$DIFF" | grep -E "$INTERFACE_REGEX")
+  // 获取本次提交和远程master分支的差异
+  exec('git diff --cached --diff-filter=A origin/main', (error, diff, stderr) => {
+    if (error) {
+      console.error(`执行错误: ${error}`);
+      return;
+    }
+    console.log(`比对获取到的差异代码：`, diff);
 
-echo $NEW_INTERFACES
-# grep -Eo "lj\.\w+\.\w+\.\w+\.\w+" "$file_to_check" | while IFS= read -r line; do
-#     # 将匹配的字符串添加到数组中
-#     matched_strings+=("$line")
-#     echo "final: ${matched_strings[*]}"
-# done
+    let matchedStrings = [];
+    const matches = diff.match(interfaceRegex) || [];
 
-# 在循环外部打印数组内容
-# echo "final: ${matched_strings[*]}"
+    const uniqueMatches = [...new Set(matches)];
+
+    console.log("final:", uniqueMatches.join(', '));
+    process.exit(1)
+  });
+});
