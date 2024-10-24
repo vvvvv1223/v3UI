139e2e7e8c1a24318bfe294383597a196f5b5df4
longjiealice_v <longjiealice_v@didiglobal.com>
2024-10-24 20:49:59 +0800

    e

--- test111.sh
+++ test111.sh
@@ -29,12 +29,6 @@ function getBranchNameAndDiff(callback) {
         return;
       }
       const diffFiles = stdout.trim().split('\n').filter(Boolean);
-      let pendingOperations = diffFiles.length;
-
-      if (diffFiles.length === 0) {
-        console.log('没有差异文件需要处理。');
-        process.exit(0);
-      }
       // 过滤出 js 和 vue 文件
       const filteredFiles = diffFiles.filter(file => file.endsWith('.js') || file.endsWith('.vue'));
       callback(null, currentBranch, filteredFiles);
@@ -49,11 +43,10 @@ function main() {
       console.error(`获取分支名或差异文件列表失败: ${error}`);
       process.exit(1);
     }
-    const interfaceRegex = /lj\.[a-zA-Z]+(\.[a-zA-Z]+)*/g;
+    const interfaceRegex = /lj\.[a-zA-Z]+(\.[a-zA-Z]+)*/g
     const uniqueMatches = new Set();
     // 匹配接口
     for (const file of diffFiles) {
-    console.log(3)
       exec(`cat ${file}`, (error, stdout, stderr) => {
         console.log(8)
         if (error) {
@@ -62,49 +55,13 @@ function main() {
         }
         // 在文件内容中匹配正则表达式
         const matches = stdout.match(interfaceRegex);
-        console.log('matches', matches)
+        console.log('matches', typeof(stdout))
         if (matches) {
           matches.forEach(match => uniqueMatches.add(match.trim()));
         }
+        
       });
       console.log(4)
-    }
-    console.log(123)
-    // 检查是否有匹配项
-    if (uniqueMatches.size) {
-      const promisesWithKeys = Array.from(uniqueMatches).map(api => {
-        return {
-          api,
-          promise: dubbo.connectMethod({
-            path: 'com.didichuxing.framework.datacenter.api.service.KopApiService',
-            method: 'queryDetail',
-            args: [
-              dubbo.java('java.lang.String', api),
-              dubbo.java('java.lang.String', '1.0.0'),
-            ]
-          })
-        };
-      });
-
-      Promise.all(promisesWithKeys.map(({ promise }) => promise)).then(results => {
-        console.log('xxx', results);
-        results.forEach((res, index) => {
-          const { api } = promisesWithKeys[index];
-          if (res && res.data && res.data.apiName) {
-            console.log(`API ${api} has data.`);
-            process.exit(1);
-          } else {
-            console.log(`API ${api} 未注册 请先注册`);
-            process.exit(1);
-          }
-        });
-      }).catch(err => {
-        console.log('fail', err);
-        process.exit(1);
-      });
-    } else {
-      console.log('增量代码没有匹配到任何接口 允许上传');
-      process.exit(1);
     }
   });
 }
